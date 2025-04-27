4cb23935c12f3cd640ff580f56d04a36b73b9542
RamboGj <jpramboguanabara@hotmail.com>
2025-04-27 01:15:53 -0300

    feat: steps

--- app/_components/StepsSection/StepsSection.tsx
+++ app/_components/StepsSection/StepsSection.tsx
@@ -1,8 +1,11 @@
-'use client'
+"use client";
 
-import { useEffect, useRef, useState } from "react"
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
-{/* 1. Sign Up and Authenticate
+import { motion } from "framer-motion";
+
+/* 1. Sign Up and Authenticate
 Description:
 Create your account quickly and securely. Choose to import your existing wallet or create a custodial wallet managed by our platform — no crypto experience needed.
 
@@ -12,90 +15,135 @@ Once you're registered, simply use your PIX key to complete your token purchase 
 
 3. Claim Your Tokens
 Description:
-After your payment is confirmed, your tokens are ready! You can claim them directly to your personal blockchain wallet or keep them safely in your custodial balance until you're ready. */}
+After your payment is confirmed, your tokens are ready! You can claim them directly to your personal blockchain wallet or keep them safely in your custodial balance until you're ready. */
 export function StepsSection() {
-    const observerRef = useRef<HTMLDivElement>(null)
-    const [progress, setProgress] = useState(0)
-  
- 
-    useEffect(() => {
-        const handleScroll = () => {
-          if (!observerRef.current) return
-    
-          const section = observerRef.current
-          const rect = section.getBoundingClientRect()
-          const windowHeight = window.innerHeight
-    
-          const start = windowHeight
-          const end = -(rect.height)
-    
-          const totalScroll = start - end
-    
-          const currentScroll = start - rect.top
-    
-          const progress = Math.min(Math.max(currentScroll / totalScroll, 0), 1)
-    
-          setProgress(progress)
-        }
-    
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-          window.removeEventListener('scroll', handleScroll)
-        }
-      }, [])
+	const observerRef = useRef<HTMLDivElement>(null);
+	const [progress, setProgress] = useState(0);
 
-    return (
- 
-<section className="div max-w-[1120px] mx-auto py-[200px]" id="HowItWorks">
-               
+	const steps = [
+		{
+			headline: "Step 1",
+			title: "Sign Up and Authenticate",
+			description:
+				"Create your account quickly and securely. Choose to import your existing wallet or create a custodial wallet managed by our platform — no crypto experience needed.",
+			imageHref: "/signup.png",
+		},
+		{
+			headline: "Step 2",
+			title: "Pay Easily via PIX",
+			description:
+				"Once you're registered, simply use your PIX key to complete your token purchase instantly. Enjoy a seamless, fast, and secure payment process designed for Brazil.",
+			imageHref: "/signup.png",
+		},
+		{
+			headline: "Step 3",
+			title: "Claim Your Tokens",
+			description:
+				"After your payment is confirmed, your tokens are ready! You can claim them directly to your personal blockchain wallet or keep them safely in your custodial balance until you're ready",
+			imageHref: "/signup.png",
+		},
+	];
 
-<h2 className="text-[5rem] text-center leading-[120%] font-ClashDisplayMedium text-white">
-  How to Buy Your AMA Tokens
-</h2>
+	useEffect(() => {
+		const handleScroll = () => {
+			if (!observerRef.current) return;
 
-<div ref={observerRef} className="flex flex-col relative gap-y-[400px]">
-<div className="w-3 bg-white/10 absolute top-12 bottom-12 left-1/2 -translate-x-1/2">
-<div
-            style={{
-              transform: `scaleY(${progress})`,
-              transformOrigin: 'top',
-            }}
-            className="absolute h-full origin-top w-3 z-20 bg-brandGradient  transition-transform duration-300 ease-out"
-          />
-  </div>
+			const section = observerRef.current;
+			const rect = section.getBoundingClientRect();
+			const windowHeight = window.innerHeight;
 
-  <div className="flex items-center justify-between mt-32">
-    <div className="max-w-[480px] flex flex-col gap-y-2 mr-auto">
-      <span className="uppercase font-GeneralSansBold text-white/50 text-[1.25rem]">Step 1</span>
-      <h3 className="text-[3rem] leading-[120%] font-ClashDisplayMedium text-green400">Sign Up and Authenticate</h3>
-      <p className="block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
-      Create your account quickly and securely. Choose to import your existing wallet or create a custodial wallet managed by our platform — no crypto experience needed.
-        </p>
-    </div>
-</div>
+			const start = windowHeight;
+			const end = -rect.height;
 
-<div className="flex items-center justify-between mt-32">
-    <div className="max-w-[480px] flex flex-col gap-y-2 ml-auto">
-      <span className="uppercase font-GeneralSansBold text-white/50 text-[1.25rem]">Step 2</span>
-      <h3 className="text-[3rem] leading-[120%] font-ClashDisplayMedium text-green400">Pay Easily via PIX</h3>
-      <p className="block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
-      Once {"you're"} registered, simply use your PIX key to complete your token purchase instantly. Enjoy a seamless, fast, and secure payment process designed for Brazil.
-        </p>
-    </div>
-</div>
+			const totalScroll = start - end;
 
-<div className="flex items-center justify-between mt-32">
-    <div className="max-w-[480px] flex flex-col gap-y-2 mr-auto">
-      <span className="uppercase font-GeneralSansBold text-white/50 text-[1.25rem]">Step 3</span>
-      <h3 className="text-[3rem] leading-[120%] font-ClashDisplayMedium text-green400">Claim Your Tokens</h3>
-      <p className="block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
-      After your payment is confirmed, your tokens are ready! You can claim them directly to your personal blockchain wallet or keep them safely in your custodial balance until {"you're "} ready.
-        </p>
-    </div>
-</div>
-</div>
+			const currentScroll = start - rect.top;
 
+			const progress = Math.min(Math.max(currentScroll / totalScroll, 0), 1);
 
-</section>
-    )
+			setProgress(progress);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	return (
+		<section className="div max-w-[1120px] mx-auto py-[200px]" id="HowItWorks">
+			<h2 className="text-[5rem] text-center leading-[120%] font-ClashDisplayMedium text-white">
+				How to Buy Your AMA Tokens
+			</h2>
+
+			<div
+				ref={observerRef}
+				className="flex flex-col relative gap-y-[400px] mt-20"
+			>
+				<div className="w-3 bg-white/10 absolute top-12 bottom-12 left-1/2 -translate-x-1/2">
+					<div
+						style={{
+							transform: `scaleY(${progress})`,
+							transformOrigin: "top",
+						}}
+						className="absolute h-full origin-top w-3 z-20 bg-brandGradient  transition-transform duration-300 ease-out"
+					/>
+				</div>
+
+				{steps.map(({ headline, imageHref, title, description }, index) => {
+					return (
+						<div
+							data-pair={index % 2 === 0}
+							key={headline}
+							className="group-step flex items-center justify-between data-[pair=true]:flex-row-reverse"
+						>
+							<div className="max-w-[480px] flex flex-col gap-y-2">
+								<motion.span
+									initial={{ opacity: 0, y: 200 }}
+									whileInView={{ opacity: 1, y: 0 }}
+									transition={{
+										duration: 0.5,
+									}}
+									viewport={{ once: true, amount: "some" }}
+									className="uppercase font-GeneralSansBold text-white/50 text-[1.25rem]"
+								>
+									{headline}
+								</motion.span>
+								<motion.h3
+									initial={{ opacity: 0, y: 200 }}
+									whileInView={{ opacity: 1, y: 0 }}
+									transition={{
+										duration: 0.5,
+									}}
+									viewport={{ once: true, amount: "some" }}
+									className="text-[3rem] leading-[120%] font-ClashDisplayMedium text-green400"
+								>
+									{title}
+								</motion.h3>
+								<motion.p
+									initial={{ opacity: 0, y: 200 }}
+									whileInView={{ opacity: 1, y: 0 }}
+									transition={{
+										duration: 0.5,
+									}}
+									viewport={{ once: true, amount: "some" }}
+									className="block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white"
+								>
+									{description}
+								</motion.p>
+							</div>
+
+							<Image
+								className="rounded-[24px] outline-2 outline-offset-[-2px] outline-white/20"
+								width={500}
+								height={500}
+								src={imageHref}
+								alt="Laptop with 3d icons floating around it"
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -6,89 +6,107 @@ import { Countdown } from "./_components/Countdown/Countdown";
 import { StepsSection } from "./_components/StepsSection/StepsSection";
 
 export default function HomePage() {
-  const navLinks = [
-    {
-      label: 'Explore',
-      href: "#"
-    },
-    {
-      label: 'Market',
-      href: "#"
-    },
+	const navLinks = [
+		{
+			label: "Explore",
+			href: "#",
+		},
+		{
+			label: "Market",
+			href: "#",
+		},
 
-    {
-      label: 'Community',
-      href: "#"
-    },
-    {
-      label: 'About',
-      href: "#"
-    },
-  ]
+		{
+			label: "Community",
+			href: "#",
+		},
+		{
+			label: "About",
+			href: "#",
+		},
+	];
 
+	return (
+		<div className="bg-[#131313]">
+			<section
+				id="hero"
+				className="bg-[url('/Elements.png')] bg-cover bg-center pb-[200px]"
+			>
+				<header className="container mx-auto">
+					<nav className="flex items-center justify-between">
+						<Link href="/" className="flex items-center gap-x-4">
+							<Image width={64} height={64} src="/logo.svg" alt="AMA Logo" />
+							<strong className="font-ClashDisplayMedium text-white text-3xl">
+								AMA
+							</strong>
+						</Link>
 
+						<ul className="flex items-center gap-x-8">
+							{navLinks.map(({ href, label }) => {
+								return (
+									<li key={label}>
+										<Link href={href}>
+											<span className="text-[1rem] font-GeneralSansMedium text-white hover:opacity-70 transtion-300 duration-500">
+												{label}
+											</span>
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
 
-  return (
-    <div className="bg-[#131313]">
-      <section id="hero" className="bg-[url('/Elements.png')] bg-cover bg-center pb-[200px]">
-        <header className="container mx-auto">
-          <nav className="flex items-center justify-between">
-            <Link href="/" className="flex items-center gap-x-4">
-              <Image width={64} height={64} src="/logo.svg" alt="AMA Logo" />
-              <strong className="font-ClashDisplayMedium text-white text-3xl">AMA</strong>
-            </Link>
+						<div className="flex items-center gap-x-6">
+							<button
+								type="button"
+								className="text-base font-GeneralSansMedium text-white hover:cursor-pointer"
+							>
+								Buy
+							</button>
+							<Link href={"/login"}>
+								<button
+									type="button"
+									className="h-[100px] flex items-center px-8 text-[#131313] font-GeneralSansMedium text-base 
+                    bg-brandGradient transition-all duration-500 hover:cursor-pointer"
+								>
+									<span>Log In</span>
+								</button>
+							</Link>
+						</div>
+					</nav>
+				</header>
 
-            <ul className="flex items-center gap-x-8">
-              {navLinks.map(({ href, label }) => {
-                return (
-                  <li key={label}>
-                    <Link href={href}>
-                      <span className="text-[1rem] font-GeneralSansMedium text-white hover:opacity-70 transtion-300 duration-500">{label}</span>
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
+				<div className="flex items-end mt-[90px] container mx-auto justify-between gap-x-24">
+					<div className="max-w-[787px]">
+						<h1 className="text-[6rem] leading-[120%] font-ClashDisplayMedium text-white">
+							Blockchain for a Sustainable Future
+						</h1>
+						<p className="max-w-[550px] block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
+							Discover how blockchain technology is driving the preservation of
+							the Amazon Rainforest and empowering communities for a sustainable
+							future. Join the movement for real impact.
+						</p>
 
-            <div className="flex items-center gap-x-6">
-              <button className="text-base font-GeneralSansMedium text-white hover:cursor-pointer">
-                Buy
-              </button>
-              <Link href={"/login"}>
-              <button className="h-[100px] flex items-center px-8 text-[#131313] font-GeneralSansMedium text-base 
-                    bg-brandGradient transition-all duration-500 hover:cursor-pointer">
-                    <span>Log In</span>
-                  </button>
-                  </Link>
-            </div>
-          </nav>
-        </header>
+						<Countdown releaseDate={new Date("04-27-2025").toISOString()} />
+					</div>
 
-        <div className="flex items-end mt-[90px] container mx-auto justify-between gap-x-24">
-          <div className="max-w-[787px]">
-            <h1 className="text-[6rem] leading-[120%] font-ClashDisplayMedium text-white">
-            Blockchain for a Sustainable Future
-            </h1>
-            <p className="max-w-[550px] block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
-            Discover how blockchain technology is driving the preservation of the Amazon Rainforest and empowering communities for a sustainable future. Join the movement for real impact.
-            </p>
-     
-            <Countdown releaseDate={new Date("04-27-2025").toISOString()} />
-          </div>
-
-
-          {/* <Canvas>
+					{/* <Canvas>
             <ambientLight intensity={0.5} />
             <directionalLight position={[5, 5, 5]} intensity={1} />
             <PingPongModel />
             <OrbitControls enableZoom={false} />
           </Canvas> */}
 
-          <video className="max-w-[1000px] w-full h-[1000px] bg-transparent absolute right-0 top-1/2 -translate-y-1/2 pointer-events-none"  muted autoPlay playsInline loop>
-            <source src="/sample.webm" />
-             {/* <source src="/morflax-studio.mp4" /> */}
-          </video>
-          {/* <div className="max-w-[445px] w-full animate-float  translate-3d perspective-[1000px]">
+					<video
+						className="max-w-[1000px] w-full h-[1000px] bg-transparent absolute right-0 top-1/2 -translate-y-1/2 pointer-events-none"
+						muted
+						autoPlay
+						playsInline
+						loop
+					>
+						<source src="/sample.webm" />
+						{/* <source src="/morflax-studio.mp4" /> */}
+					</video>
+					{/* <div className="max-w-[445px] w-full animate-float  translate-3d perspective-[1000px]">
             <div className="bg-[#D9D9D9] w-full h-[454px]">
 
             </div>
@@ -111,44 +129,59 @@ export default function HomePage() {
               </div>
             </div>
           </div> */}
-        </div>
-      </section>
-   
-            <section className="py-[200px] relative overflow-hidden" id="second">
-              <div className="absolute right-0 bottom-0 w-[468px] h-[312px] rotate-[-25deg] blur-[256px] rounded-[468px] bg-[rgba(15,248,136,0.35)] z-0" />
-              <div className="absolute left-0 top-0 w-[468px] h-[312px] rotate-[-25deg] blur-[256px] rounded-[468px] bg-[rgba(15,248,136,0.35)] z-0" />
-            <div className="max-w-[1120px] mx-auto">
-            <h2 className="text-[5rem] leading-[120%] font-ClashDisplayMedium text-white">
-             Secure Your Tokens Early and Save
-            </h2>
-              <p className="max-w-[550px] block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
-                Check out each ICO phase and lock in the best prices:
-              </p>
+				</div>
+			</section>
 
+			<section className="py-[200px] relative overflow-hidden" id="second">
+				<div className="absolute right-0 bottom-0 w-[468px] h-[312px] rotate-[-25deg] blur-[256px] rounded-[468px] bg-[rgba(15,248,136,0.35)] z-0" />
+				<div className="absolute left-0 top-0 w-[468px] h-[312px] rotate-[-25deg] blur-[256px] rounded-[468px] bg-[rgba(15,248,136,0.35)] z-0" />
+				<div className="max-w-[1120px] mx-auto">
+					<h2 className="text-[5rem] leading-[120%] font-ClashDisplayMedium text-white">
+						Secure Your Tokens Early and Save
+					</h2>
+					<p className="max-w-[550px] block mt-4 text-[1.125rem] leading-[160%] font-GeneralSansRegular text-white">
+						Check out each ICO phase and lock in the best prices:
+					</p>
 
-              <ul className="w-full flex flex-col mt-12 z-20">
-                <li className="bg-[rgba(14,62,52,0.2)] w-full px-6 rounded-[12px] h-[104px] flex justify-between items-center gap-x-6">
-                  <strong className="text-[#FAFF00] font-ClashDisplayMedium text-[1.5rem]">1. Seed :</strong>
-                  <span className="text-[1.25rem] text-white font-GeneralSansMedium">R$X.XX / Token </span>
-                  <span className="text-[1.25rem] text-white font-GeneralSansMedium">Hard Cap: R$XXX,XXX</span>
-                </li>
-                <li className="-full px-6 rounded-[12px] h-[104px] flex justify-between items-center gap-x-6">
-                  <strong className="text-[#FAFF00] font-ClashDisplayMedium text-[1.5rem]">2. Private :</strong>
-                  <span className="text-[1.25rem] text-white font-GeneralSansMedium">R$X.XX / Token </span>
-                  <span className="text-[1.25rem] text-white font-GeneralSansMedium">Hard Cap: R$XXX,XXX</span>
-                </li>
-                <li className="bg-[rgba(14,62,52,0.2)] w-full px-6 rounded-[12px] h-[104px] flex justify-between items-center gap-x-6">
-                  <strong className="text-[#FAFF00] font-ClashDisplayMedium text-[1.5rem]">3. Public :</strong>
-                  <span className="text-[1.25rem] text-white font-GeneralSansMedium">R$X.XX / Token </span>
-                  <span className="text-[1.25rem] text-white font-GeneralSansMedium">Hard Cap: R$XXX,XXX</span>
-                </li>
-              </ul>
-            </div>
-     
-            </section>
+					<ul className="w-full flex flex-col mt-12 z-20">
+						<li className="bg-[rgba(14,62,52,0.2)] w-full px-6 rounded-[12px] h-[104px] flex justify-between items-center gap-x-6">
+							<strong className="text-[#FAFF00] font-ClashDisplayMedium text-[1.5rem]">
+								1. Seed :
+							</strong>
+							<span className="text-[1.25rem] text-white font-GeneralSansMedium">
+								R$X.XX / Token{" "}
+							</span>
+							<span className="text-[1.25rem] text-white font-GeneralSansMedium">
+								Hard Cap: R$XXX,XXX
+							</span>
+						</li>
+						<li className="-full px-6 rounded-[12px] h-[104px] flex justify-between items-center gap-x-6">
+							<strong className="text-[#FAFF00] font-ClashDisplayMedium text-[1.5rem]">
+								2. Private :
+							</strong>
+							<span className="text-[1.25rem] text-white font-GeneralSansMedium">
+								R$X.XX / Token{" "}
+							</span>
+							<span className="text-[1.25rem] text-white font-GeneralSansMedium">
+								Hard Cap: R$XXX,XXX
+							</span>
+						</li>
+						<li className="bg-[rgba(14,62,52,0.2)] w-full px-6 rounded-[12px] h-[104px] flex justify-between items-center gap-x-6">
+							<strong className="text-[#FAFF00] font-ClashDisplayMedium text-[1.5rem]">
+								3. Public :
+							</strong>
+							<span className="text-[1.25rem] text-white font-GeneralSansMedium">
+								R$X.XX / Token{" "}
+							</span>
+							<span className="text-[1.25rem] text-white font-GeneralSansMedium">
+								Hard Cap: R$XXX,XXX
+							</span>
+						</li>
+					</ul>
+				</div>
+			</section>
 
-
-           <StepsSection />
-    </div>
-  )
+			<StepsSection />
+		</div>
+	);
 }
